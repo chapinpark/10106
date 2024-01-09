197ea71f657bfbbfd16128d5f715bef77b1812ac
chapinpark <jspeaks@nd.edu>
2024-01-09 10:53:22 -0500

    updated root json file and modifided path to build folder in server.js

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.use('/api', updateTablesRouter);
 console.log('updateTablesRouter has been registered');
 
 // Serve React App in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
-  });
-}
+
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 
 const PORT = process.env.PORT || 4000;
